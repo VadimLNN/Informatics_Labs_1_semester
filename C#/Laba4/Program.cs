52084452e48530ff93f8c9ed4a47c66f5e4920ef
VadimBALS <svobodolubsev@inbox.ru>
2022-11-16 18:32:33 +0700

    Генерация password в функции есть

--- C#/Laba4/Program.cs
+++ C#/Laba4/Program.cs
@@ -8,10 +8,38 @@ namespace Laba4
 {
     internal class Program
     {
+        static string GenPassword()
+        {
+            Random rng = new Random();
+
+            Console.Write("Enter password lenght: ");
+            int l = int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter start range: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("Enter end range: ");
+            int end = int.Parse(Console.ReadLine());
+
+            string password = "";
+
+            for (int i = 0; i < l; i++)
+            {
+                char ch = (char)rng.Next(start, end);
+                password += ch;
+            }
+
+            return "password: " + password; 
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(GenPassword());
 
 
+
+
+
+            Console.ReadKey();
         }
     }
 }
