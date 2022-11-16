a0cbda1df984c4b6381b118c3b27f6ff37c2ac06
VadimBALS <svobodolubsev@inbox.ru>
2022-11-16 19:46:28 +0700

    шифровка по цезарю дешифровка по цезарю но введённого в консоль выражения

--- C#/Laba4/Program.cs
+++ C#/Laba4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,14 +31,45 @@ namespace Laba4
 
             return "password: " + password; 
         }
+        // генерим пaроль
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine(GenPassword());
+            //Console.WriteLine(GenPassword());
 
 
 
+            Console.WriteLine("Enter text:");
+            string text = Console.ReadLine();
 
+            Console.Write("Enter shift:");
+            int sh = int.Parse(Console.ReadLine());
+
+            // шифрование по цезарю
+            char[] encoded = new char[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+                encoded[i] = (char)((((byte)text[i]) + sh) % 256);
+
+            // дешифрование по цезарю
+            char[] decoded = new char[text.Length];
+
+            for (int i = 0; i < encoded.Length; i++)
+            {
+                int t = (int)((byte)(encoded[i] - sh));
+
+                if (t < 0) t += 255;
+
+                decoded[i] = (char)t;
+            }
+
+            // вывод на экран
+            for (int i = 0; i < encoded.Length; i++)
+                Console.Write(encoded[i]);
+            Console.WriteLine();
+            for (int i = 0; i < decoded.Length; i++)
+                Console.Write(decoded[i]);
 
             Console.ReadKey();
         }
